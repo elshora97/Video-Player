927e49f24c8e1df712c0644c029fb07927c13938
elshora97 <ahmedashrafrlshora@gmail.com>
2022-06-17 14:07:05 +0200

    declation remaining functions

--- script.js
+++ script.js
@@ -19,10 +19,35 @@ function updatePlayIcon() {
         play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
     }else {
         play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-        console.log ('asa')
+       
     }
 }
 
+//update progress 
+
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+      //get minutes 
+  let mins = Math.floor(video.currentTime /60);
+  if(mins < 10){
+    mins = '0'+ String(mins);
+  }
+
+  //get sec
+  let sec = Math.floor(video.currentTime % 60);
+  if(sec < 10){
+    sec = '0'+ String(sec);
+  }
+
+  timestamp.innerHTML = `${mins}:${sec}`;
+}
+
+//set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration)/100;
+
+}
 
 // stop videos 
 function stopVideo(){
@@ -31,17 +56,16 @@ function stopVideo(){
 }
 
 
-
 //event listeners
 
 video.addEventListener('click' , toggleVideoStatus);
 video.addEventListener('pause' , updatePlayIcon);
 video.addEventListener('play' , updatePlayIcon);
-// video.addEventListener('timeupdate' , updateProgress);
+video.addEventListener('timeupdate' , updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
 stop.addEventListener('click' , stopVideo);
 
-// progress.addEventListener('change', setVideoProgress);
+progress.addEventListener('change', setVideoProgress);
 
